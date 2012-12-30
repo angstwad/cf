806c37c857900326b89721fba6656716b12f0ab1
Paul Durivage <pauldurivage@gmail.com>
2012-12-30 15:03:54 -0600

    small changes

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='cf',
     version='0.1',
     description='cf -- A CLI to Cloud Files',
     author='Paul Durivage',
     author_email='pauldurivage@gmail.com',
-    url='',
-    modules = {'python-cloudfiles': 'cloudfiles'},
+    license='Apache 2.0',
+    url='https://github.com/angstwad/cf',
+    install_requires = ['python-cloudfiles'],
     scripts = ['bin/cf']
 )
