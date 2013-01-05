70cf2139fce712816fff5d31ef6fffb3ad60f3f5
Paul <pauldurivage@gmail.com>
2013-01-04 21:17:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@
 from setuptools import setup
 
 setup(name='cf',
-<<<<<<< HEAD
     version='0.20',
-=======
-    version='0.2.0',
->>>>>>> 5812b353482624694730085773a81de598a0111c
     description='A command line client to Rackspace Cloud Files',
     author='Paul Durivage',
     author_email='pauldurivage@gmail.com',
